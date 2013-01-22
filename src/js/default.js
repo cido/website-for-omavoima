c52cf4b1f16165ceca44cc5896e5733bb744d6f7
Jouni Kaplas <jouni.kaplas@iki.fi>
2013-01-22 23:37:31 +0200

    Improved block handling quite a much.

--- src/js/default.js
+++ src/js/default.js
@@ -6,16 +6,18 @@
 			var self = this;
 
 			var logo = document.getElementById('logo');
-			var slogan = document.getElementById('slogan');
-			var sloganTestingArea = this._addSloganTestingArea(slogan);
+			//var slogan = document.getElementById('slogan');
+			//var sloganTestingArea = this._addSloganTestingArea(slogan);
+
+			this._detectCssSupportFor('box-shadow');
 
 			if (this._supportsSvg() === false) {
 				logo.setAttribute('src', logo.getAttribute('data-fallback-src'));
 			}
 
-			var listener = function () { self._onResize(logo, slogan, sloganTestingArea); };
-			window.addEventListener('resize', listener);
-			listener();
+			//var listener = function () { self._onResize(logo, slogan, sloganTestingArea); };
+			//window.addEventListener('resize', listener);
+			//listener();
 
 		},
 
@@ -40,6 +42,29 @@
 		_supportsSvg: function () {
 			return !! document.createElementNS &&
 				!! document.createElementNS ('http://www.w3.org/2000/svg', "svg").createSVGRect;
+		},
+
+		// Adapted from http://www.sitepoint.com/detect-css3-property-browser-support/
+		_detectCssSupportFor: function (cssPropertyWithDashes) {
+			var cssProperty1 = this._toCamelCase(cssPropertyWithDashes);
+			var cssProperty2 = this._capitaliseFirstLetter(cssProperty1);
+			var d = document.createElement("detect");
+			var cssPrefixes = ["Webkit", "Moz", "O", "ms", "Khtml"];
+			var checkThese = (cssProperty1 + "," + cssPrefixes.join(cssProperty2 + ",") + cssProperty2).split(",");
+			for (var n = 0, np = checkThese.length; n < np; n++) {
+				if (d.style[checkThese[n]] === "") {
+					document.getElementsByTagName("html")[0].className += " " + cssPropertyWithDashes;
+					break;
+				}
+			}
+		},
+
+		_toCamelCase: function (str) {
+			return str.replace(/(\-[a-z])/g, function($1){return $1.toUpperCase().replace('-','');});
+		},
+
+		_capitaliseFirstLetter: function(str) {
+			return str.charAt(0).toUpperCase() + str.slice(1);
 		}
 
 	};
