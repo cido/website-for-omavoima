dcf4f5ba3ae7ed2aeec39db5a1eba57444aa5d9b
Jouni Kaplas <jouni.kaplas@iki.fi>
2013-07-31 22:57:31 +0300

    Automated site generation

---
+++ YALRfile.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+  // Watch files only in the public directory
+  path: "src",
+
+  // Reload page only when html, css or js file changes
+  match: [
+    "*.html",
+    "*.css",
+    "*.js"
+  ]
+
+};
